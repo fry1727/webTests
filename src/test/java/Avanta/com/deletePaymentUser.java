440116da84cc0e0d0ed968227625c41b36bd7e93
Yauheni skiruk <fry1727@gmail.com>
2019-12-04 18:25:21 +0300

    work 04.12.2019

--- src/test/java/Avanta/com/deletePaymentUser.java
+++ src/test/java/Avanta/com/deletePaymentUser.java
@@ -1,12 +1,11 @@
 package Avanta.com;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -16,21 +15,20 @@ import java.util.Map;
 public class deletePaymentUser {
 
     private WebDriver driver;
-    private Map<String, Object> vars;
-    JavascriptExecutor js;
+    // private Map<String, Object> vars;
+    //JavascriptExecutor js;
 
 
     @Before
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "src/recources/chromedriver.exe");
-        driver = new ChromeDriver();
-        js = (JavascriptExecutor) driver;
-        vars = new HashMap<String, Object>();
 
+        ChromeOptions options = new ChromeOptions();
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        prefs.put("profile.default_content_setting_values.notifications", 2); //1-Allow, 2-Block, 0-default
+        options.setExperimentalOption("prefs", prefs);
+        driver = new ChromeDriver(options);
 
-        //ChromeOptions options = new ChromeOptions();
-        //options.addArguments("user-agent=\"Mozilla/5.0 (iPhone; CPU iPhone OS 11_2_1 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) CriOS/63.0.3239.73 Mobile/15C153 Safari/604.1\"");
-        //new ChromeDriver(options);
     }
 
     @Test
@@ -48,23 +46,24 @@ public class deletePaymentUser {
         String passwordPayment = "111111";  // удалить после нормальной реализации
         driver.findElement(By.name("password")).sendKeys(passwordPayment);
         driver.findElement(By.cssSelector("button[class*='Button']")).click();
-        new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.className("text-title-medium")));
+
+        new WebDriverWait(driver, 20).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.id("Layer_1___1847921000")));
         driver.findElement(By.id("Layer_1___1847921000")).click();
-        new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.cssSelector("a[href*='/myProfile']")));
+        new WebDriverWait(driver, 20).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("a[href*='/myProfile']")));
         driver.findElement(By.cssSelector("a[href*='/myProfile']")).click();
-        new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.cssSelector("a[href*='/settings'")));
-        driver.findElement(By.cssSelector("a[href*='/settings'")).click();
-        new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.cssSelector("a[href*='/settings/accountSettings")));
-        driver.findElement(By.cssSelector("a[href*='/settings/accountSettings")).click();
-        new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.cssSelector("a[href*='/settings/accountSettings/changeEmail")));
-        driver.findElement(By.cssSelector("a[href*='/settings/accountSettings/changeEmail")).click();
+        new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.cssSelector("a[href*='/settings']")));
+        driver.findElement(By.cssSelector("a[href*='/settings']")).click();
+        new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.cssSelector("a[href*='/settings/accountSettings']")));
+        driver.findElement(By.cssSelector("a[href*='/settings/accountSettings']")).click();
+        new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.cssSelector("a[href*='/settings/accountSettings/changeEmail']")));
+        driver.findElement(By.cssSelector("a[href*='/settings/accountSettings/changeEmail']")).click();
 
     }
 
-    @After
-    public void tearDown() {
-        driver.quit();
-    }
+    //  @After
+    //public void tearDown() {
+    //  driver.quit();
+    //}
 }
 
 
