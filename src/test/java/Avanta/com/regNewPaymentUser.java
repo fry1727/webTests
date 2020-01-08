d4a957e37823401a1c614db0fb2f2c43f6255ce9
Yauheni Skiruk <fry1727@gmail.com>
2020-01-09 00:44:30 +0300

    09/01/2020 home part

--- src/test/java/Avanta/com/regNewPaymentUser.java
+++ src/test/java/Avanta/com/regNewPaymentUser.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,7 +31,7 @@ public class regNewPaymentUser {
     }
 
     @Test
-    public void createNewUserForPayment() {
+    public void createNewUserForPayment() throws AWTException {
 
         driver.get("https://m.meetville.com/");
         new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.cssSelector(".px-3 > svg")));
@@ -56,23 +58,31 @@ public class regNewPaymentUser {
         //заполнение describe yourself
         new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(text(),'Chat')]")));
         driver.findElement(By.xpath("//span[contains(text(),'Chat')]")).click();
+        Robot robot = new Robot();
+        robot.keyPress(KeyEvent.VK_PAGE_DOWN);
+        robot.keyRelease(KeyEvent.VK_PAGE_DOWN);
         new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(text(),'Never married')]")));
         driver.findElement(By.xpath("//span[contains(text(),'Never married')]")).click();
-        new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//span[contains(text(),'No')]")));
-        driver.findElement(By.xpath("//span[contains(text(),'No')]")).click();
-        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.cssSelector("#wantsKids .item-with-line:nth-child(3) > .PortayYourselfControl__question_list_item___1SJIM")));
-        driver.findElement(By.cssSelector("#wantsKids .item-with-line:nth-child(3) > .PortayYourselfControl__question_list_item___1SJIM")).click();
-        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.cssSelector("#ethnicity .item-with-line:nth-child(5) > .PortayYourselfControl__question_list_item___1SJIM")));
-        driver.findElement(By.cssSelector("#ethnicity .item-with-line:nth-child(5) > .PortayYourselfControl__question_list_item___1SJIM")).click();
-        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.cssSelector(".item-with-line:nth-child(7) > .PortayYourselfControl__question_list_item___1SJIM")));
-        driver.findElement(By.cssSelector(".item-with-line:nth-child(7) > .PortayYourselfControl__question_list_item___1SJIM")).click();
-        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.cssSelector("#education .item-with-line:nth-child(4) > .PortayYourselfControl__question_list_item___1SJIM")));
-        driver.findElement(By.cssSelector("#education .item-with-line:nth-child(4) > .PortayYourselfControl__question_list_item___1SJIM")).click();
-        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.cssSelector("#bodyType .item-with-line:nth-child(3) > .PortayYourselfControl__question_list_item___1SJIM")));
-        driver.findElement(By.cssSelector("#bodyType .item-with-line:nth-child(3) > .PortayYourselfControl__question_list_item___1SJIM")).click();
-        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.cssSelector("#smokes .item-with-line:nth-child(2) > .PortayYourselfControl__question_list_item___1SJIM")));
-        driver.findElement(By.cssSelector("#smokes .item-with-line:nth-child(2) > .PortayYourselfControl__question_list_item___1SJIM")).click();
-        driver.findElement(By.cssSelector(".item-with-line:nth-child(2) > .PortayYourselfControl__question_list_item___1SJIM")).click();
+        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='kidsAtHome']//span[contains(text(),'No')]")));
+        driver.findElement(By.xpath("//div[@id='kidsAtHome']//span[contains(text(),'No')]")).click();
+        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='wantsKids']//span[contains(text(),'No')]")));
+        driver.findElement(By.xpath("//div[@id='wantsKids']//span[contains(text(),'No')]")).click();
+        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(text(),'Asian')]")));
+        driver.findElement(By.xpath("//span[contains(text(),'Asian')]")).click();
+        robot.keyPress(KeyEvent.VK_PAGE_DOWN);
+        robot.keyRelease(KeyEvent.VK_PAGE_DOWN);
+
+        
+        new WebDriverWait(driver, 10).until(ExpectedConditions.ele(By.xpath("//span[contains(text(),'Agnostic')]")));
+        driver.findElement(By.xpath("//span[contains(text(),'Agnostic')]")).click();
+        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeSelected(By.xpath("//span[contains(text(),'No Degree')]")));
+        driver.findElement(By.xpath("//span[contains(text(),'No Degree')]")).click();
+        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(text(),'Average')]")));
+        driver.findElement(By.xpath("//span[contains(text(),'Average')]")).click();
+        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='smokes']//span[contains(text(),'No')]")));
+        driver.findElement(By.xpath("//div[@id='smokes']//span[contains(text(),'No')]")).click();
+        new WebDriverWait(driver, 5).until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='drinks']//span[contains(text(),'No')]")));
+        driver.findElement(By.xpath("//div[@id='drinks']//span[contains(text(),'No')]")).click();
         driver.findElement(By.cssSelector(".RulerScale__inner___2QpaF > svg")).click();
         driver.findElement(By.cssSelector(".text-small-caption-regular > span")).click();
         driver.findElement(By.cssSelector(".RulerScale__inner___2QpaF")).click();
@@ -95,7 +105,7 @@ public class regNewPaymentUser {
         //photoUpload
 
 
-        new WebDriverWait(driver,10).until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='PhotoBox__container___axuMm PhotoBox__empty___3Po31']")));
+        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='PhotoBox__container___axuMm PhotoBox__empty___3Po31']")));
         driver.findElement(By.xpath("//input[@class='display_none']")).sendKeys("C:\\maven\\webTests\\src\\recources\\IMG_0784.JPG");
         new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeSelected(By.cssSelector(".SaveContinueButton__footer_fixed___3D1EY .Button__button___2GX_6")));
         driver.findElement(By.xpath("//span[contains(text(),'Save & Continue']")).click();
