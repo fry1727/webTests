9477663c6e0e1cadb2089df03bcdd08a1d844e67
fry1727 <fry1727@gmail.com>
2020-06-17 01:13:35 +0300

    add RegistrationMainPage test

---
+++ src/test/java/PageObjects/Pages/RegistrationMainPage.java
@@ -0,0 +1,128 @@
+package PageObjects.Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class RegistrationMainPage {
+
+    private static WebElement element = null;
+
+    public static WebElement registrationMainImAManButton(WebDriver driver) {
+        element = driver.findElement(By.xpath("//*[@id=\"app-root\"]/main/div/main/div/div/div[2]/div/button[1]"));
+        return element;
+    }
+
+    public static WebElement registrationMainImWomanButton(WebDriver driver) {
+        element = driver.findElement(By.xpath("//*[@id=\"app-root\"]/main/div/main/div/div/div[2]/div/button[2]"));
+        return element;
+    }
+
+    public static WebElement registrationMainTextLetsFindYour(WebDriver driver) {
+        element = driver.findElement(By.xpath("//*[@id=\"app-root\"]/main/div/main/div/div/div[1]/h5/span[1]"));
+        return element;
+    }
+
+    public static WebElement registrationMainTextNextDate(WebDriver driver) {
+        element = driver.findElement(By.xpath("//*[@id=\"app-root\"]/main/div/main/div/div/div[1]/h5/span[2]"));
+        return element;
+    }
+
+    public static WebElement registrationMainTextAreYouManOrWoman(WebDriver driver) {
+        element = driver.findElement(By.xpath("//*[@id=\"app-root\"]/main/div/main/div/div/div[2]/p[1]/span"));
+        return element;
+    }
+
+    public static WebElement registrationMainTextAlreadyHasAnAcc(WebDriver driver) {
+        element = driver.findElement(By.xpath("//*[@id=\"app-root\"]/main/div/main/div/div/div[2]/p[2]/span"));
+        return element;
+    }
+
+    public static WebElement registrationMainSignIn(WebDriver driver) {
+        element = driver.findElement(By.xpath("//*[@id=\"app-root\"]/main/div/main/div/div/div[2]/p[2]/a/span"));
+        return element;
+    }
+
+    public static WebElement registrationMain2WhoAreYouSeeking(WebDriver driver) {
+        element = driver.findElement(By.xpath("//*[@id=\"app-root\"]/main/div/main/div/div/div/p[1]/span"));
+        return element;
+    }
+
+    public static WebElement registrationMain2LookingForFirstButton(WebDriver driver) {
+        element = driver.findElement(By.xpath("//*[@id=\"app-root\"]/main/div/main/div/div/div/div/button[1]"));
+        return element;
+    }
+
+    public static WebElement registrationMain2LookingforSecondButton(WebDriver driver) {
+        element = driver.findElement(By.xpath("//*[@id=\"app-root\"]/main/div/main/div/div/div/div/button[2]"));
+        return element;
+    }
+
+    public static WebElement registrationMain3WhenIsYourBirthday(WebDriver driver) {
+        element = driver.findElement(By.xpath("//*[@id=\"app-root\"]/main/div/main/div/div/p/span"));
+        return element;
+    }
+
+    public static WebElement registrationMain3BirthdayInput(WebDriver driver) {
+        element = driver.findElement(By.xpath("//*[@id=\"app-root\"]/main/div/main/div/div/div[1]/input"));
+        return element;
+    }
+
+    public static WebElement registrationMain3ContinueButton(WebDriver driver) {
+        element = driver.findElement(By.xpath("//*[@id=\"app-root\"]/main/div/main/div/div/div[2]/button"));
+        return element;
+    }
+
+    public static WebElement registrationMain3DateOkButton(WebDriver driver) {
+        element = driver.findElement(By.xpath("//*[@id=\"app-root\"]/main/div/main/div/div/div[1]/div/div/div[2]/button[2]/span"));
+        return element;
+    }
+
+    public static WebElement registrationMain4WhatIsYourNickname(WebDriver driver) {
+        element = driver.findElement(By.xpath("//*[@id=\"app-root\"]/main/div/main/div/div/p/span"));
+        return element;
+    }
+
+    public static WebElement registrationMain4NicknameInput(WebDriver driver) {
+        element = driver.findElement(By.xpath("//*[@id=\"app-root\"]/main/div/main/div/div/form/label/input"));
+        return element;
+    }
+
+    public static WebElement registrationMain4ContinueButton(WebDriver driver) {
+        element = driver.findElement(By.xpath("//*[@id=\"app-root\"]/main/div/main/div/div/form/div/button"));
+        return element;
+    }
+
+    public static WebElement registrationMain5WhatIsYourEmail(WebDriver driver) {
+        element = driver.findElement(By.xpath("//*[@id=\"app-root\"]/main/div/main/div/div/p[1]/span"));
+        return element;
+    }
+
+    public static WebElement registrationMain5EmailInput(WebDriver driver) {
+        element = driver.findElement(By.xpath("//*[@id=\"app-root\"]/main/div/main/div/div/form/label[1]/input"));
+        return element;
+    }
+
+    public static WebElement registrationMain5CheckboxEmail(WebDriver driver) {
+        element = driver.findElement(By.xpath("//*[@id=\"app-root\"]/main/div/main/div/div/form/label[2]"));
+        return element;
+    }
+
+    public static WebElement registrationMain5CheckboxEmailText(WebDriver driver) {
+        element = driver.findElement(By.xpath("//*[@id=\"app-root\"]/main/div/main/div/div/form/label[2]/span[2]/span"));
+        return element;
+    }
+
+    public static WebElement registrationMain5PrivacyCookieTermsText(WebDriver driver) {
+        element = driver.findElement(By.xpath("//*[@id=\"app-root\"]/main/div/main/div/div/p[2]"));
+        return element;
+    }
+
+    public static WebElement registrationMain5ContinueButton (WebDriver driver) {
+        element = driver.findElement(By.xpath("//*[@id=\"app-root\"]/main/div/main/div/div/form/div/div/button"));
+        return element;
+    }
+
+
+}
+//
